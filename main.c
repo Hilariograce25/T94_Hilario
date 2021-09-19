2eef62b0f0bc9192996be5b376ae1c00d0593d2f
Funny Grace P. Hilario <89248995+Hilariograce25@users.noreply.github.com>
2021-09-19 11:30:26 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
     //declaration of variables//
     int num1 = 19;
     //printing of Variables//
-    printf("I'm Funny Grace Hilario, age of %d years old",num1);
+    printf("I'm Funny Grace Hilario, Age of %d years old",num1);
     return 0;
 }
