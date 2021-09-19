a6aec60d7ef419a42bccad5dd89880df0771806e
Funny Grace P. Hilario <89248995+Hilariograce25@users.noreply.github.com>
2021-09-20 02:28:10 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //declaration of variables//
+    int num1 = 19;
+    //printing of Variables//
+    printf("I'm Funny Grace Hilario, age of %d years old",num1);
+    return 0;
+}
