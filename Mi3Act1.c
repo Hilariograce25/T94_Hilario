9faf02fda59486bcd6c19cba7b3050f616b49013
Funny Grace P. Hilario <89248995+Hilariograce25@users.noreply.github.com>
2021-10-19 02:25:50 -0700

    Update Mi3Act1.c

--- Mi3Act1.c
+++ Mi3Act1.c
@@ -10,7 +10,7 @@ printf("Very cold weather.\n");
 else if (temp<20)
 printf("Cold weather.\n");
 else if(temp<30)
-printf("Normal weather.\n");
+printf("Normal in Temperature.\n");
 else if(temp<40)
 printf("Its Hot.\n");
 else if(temp>40)
