7e6f58898f0df9285827c6d0994c2a89b72719ef
Funny Grace P. Hilario <89248995+Hilariograce25@users.noreply.github.com>
2021-10-19 02:09:11 -0700

    Add files via upload

---
+++ Mi3Act1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{ int temp;
+printf("Input temperature:");
+scanf("%d",&temp);
+if (temp<0)
+printf("Freezing weather.\n");
+else if (temp<10)
+printf("Very cold weather.\n");
+else if (temp<20)
+printf("Cold weather.\n");
+else if(temp<30)
+printf("Normal weather.\n");
+else if(temp<40)
+printf("It's Hot.\n");
+else if(temp>40)
+printf("It's Very Hot.\n");
+}
